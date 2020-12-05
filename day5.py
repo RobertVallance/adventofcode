e3296f2a813f3baa358e494d053dfc1711aa2f89
RobertVallance <robert.vallance123@gmail.com>
2020-12-05 15:41:59 +0000

    Small fix of indentations on docstring of first function in day 5 and a comment fix

--- day5.py
+++ day5.py
@@ -9,24 +9,24 @@ def get_seat_number_from_boarding_string(boarding_string):
 	B in row_string means back  (2nd half of row range)
 	F in row_string means front (1st half of column range)
 	B in row_string means back  (2nd half of column range)
-	
-	For example, consider the row_string FBFBBFFRLR:
-    
-    First 7 chars:
-    - Start by considering the whole range, rows 0 through 127
-    - F means to take the lower half, keeping rows 0 through 63
-    - B means to take the upper half, keeping rows 32 through 63
-    - F means to take the lower half, keeping rows 32 through 47
-    - B means to take the upper half, keeping rows 40 through 47
-    - B keeps rows 44 through 47
-    - F keeps rows 44 through 45
-    - The final F keeps the lower of the two, row 44
+
+	For example, consider the boarding_string FBFBBFFRLR:
+
+	First 7 chars:
+	- Start by considering the whole range, rows 0 through 127
+	- F means to take the lower half, keeping rows 0 through 63
+	- B means to take the upper half, keeping rows 32 through 63
+	- F means to take the lower half, keeping rows 32 through 47
+	- B means to take the upper half, keeping rows 40 through 47
+	- B keeps rows 44 through 47
+	- F keeps rows 44 through 45
+	- The final F keeps the lower of the two, row 44
 
 	Last 3 chars:
-    - Start by considering the whole range, columns 0 through 7
-    - R means to take the upper half, keeping columns 4 through 7
-    - L means to take the lower half, keeping columns 4 through 5
-    - The final R keeps the upper of the two, column 5
+	- Start by considering the whole range, columns 0 through 7
+	- R means to take the upper half, keeping columns 4 through 7
+	- L means to take the lower half, keeping columns 4 through 5
+	- The final R keeps the upper of the two, column 5
 
 
 	Parameters:
